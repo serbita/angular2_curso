913202ad4b6c25a1723f52e9d9e65e6e9fb2473e
Sebastian Menel <serbita@gmail.com>
2017-01-25 14:45:40 -0300

    Init

---
+++ epanteAngular2Test/src/app/app-routing.module.ts
@@ -0,0 +1,81 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PageNotFoundComponent }    from './not-found.component';
+import { HomeComponent }            from './home/home.component';
+import { PruebasComponent }         from './pruebas/pruebas.component';
+import { CustomerListComponent }    from './customers/customer-list.component';
+import { CustomerDetailComponent }    from './customers/customer-detail.component';
+
+
+/*import { CustomersComponent } from './customers.component';
+import { ComposeMessageComponent }  from './compose-message.component';
+import { CanDeactivateGuard }       from './can-deactivate-guard.service';
+import { AuthGuard }                from './auth-guard.service';
+*/
+
+import { SelectivePreloadingStrategy }   from './selective-preloading-strategy';
+
+
+const appRoutes: Routes = [
+ 
+  { path: 'home', component: HomeComponent },
+  //sacar el path de home porque debería ser atendido por el modulo de routing de home
+
+  { path: 'customers', component: CustomerListComponent,
+    children: [
+      //{ path: '', redirectTo: 'overview', pathMatch: 'full' },    
+      { path: ':id', component: CustomerDetailComponent },
+      { path: 'otro', component: CustomerDetailComponent }
+    ]
+  },  
+/*
+  {
+    path: 'customers',
+    loadChildren: 'app/customers/customers.module#CustomersModule',
+    data: { preload: true }
+  },
+*/  
+
+  { path: 'pruebas', component: PruebasComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },  
+  { path: '**', component: PageNotFoundComponent }
+ 
+   /*,
+  {
+    path: 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
+  {
+    path: 'admin',
+    loadChildren: 'app/admin/admin.module#AdminModule',
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'crisis-center',
+    loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule',
+    data: { preload: true }
+  },
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+*/  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+      /*,{ preloadingStrategy: SelectivePreloadingStrategy }*/
+    )
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+  /*  SelectivePreloadingStrategy,
+    CanDeactivateGuard
+  */
+  ]
+})
+export class AppRoutingModule {}
---
+++ epanteAngular2Test/src/app/customers/customer-detail.component.ts
@@ -0,0 +1,64 @@
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+//import { slideInDownAnimation } from '../animations';
+
+import { Customer, CustomerService }  from './customer.service';
+//import { Customer }  from './customer.service';
+
+
+@Component({
+  template: `
+  <h2>CUSTOMERS</h2>
+  <div *ngIf="customer">
+    <h3>"{{ customer.name }}"</h3>
+    <div>
+      <label>Id: </label>{{ customer.id }}</div>
+    <div>
+      <label>Name: </label>
+      <input [(ngModel)]="customer.name" placeholder="name"/>
+    </div>
+    <p>
+      <button (click)="gotoCustomers()">Back</button>
+    </p>
+  </div>
+  
+  `//,  providers: [CustomerService],
+  //animations: [ slideInDownAnimation ]
+})
+export class CustomerDetailComponent implements OnInit {
+  
+/*  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'absolute';
+*/
+  customer: Customer;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: CustomerService
+  ) {}
+
+	ngOnInit() {
+		this.route.params
+		// (+) converts string 'id' to a number
+		.switchMap((params: Params) => this.service.getCustomer(+params['id']))
+		.subscribe((customer: Customer) => this.customer = customer);
+  	}
+
+  gotoCustomers() {
+		let customerId = this.customer ? this.customer.id : null;
+	    // Pass along the customer id if available
+	    // so that the CustomerList component can select that customer.
+	    // Include a junk 'foo' property for fun.
+		this.router.navigate(['/customers', { id: customerId, foo: 'foo' }]);
+	}
+
+}
+
+
+
+
+
